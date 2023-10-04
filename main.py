df9f9ca5d707652f6023add43dd6c17c1099d2b6
GuFFy12 <jokers_hello_0d@icloud.com>
2023-10-04 18:53:54 +0500

    Beautify code and add main function; Better console logs; Add errors printing in get_event_color and get_lesson_num functions, when values in dictionary not found.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from gcsa.google_calendar import GoogleCalendar
 # Utilities
 
 def get_first_day_of_first_september_week(current_date: datetime):
-    first_september = pytz.timezone('Asia/Yekaterinburg').localize(datetime(current_date.year, 9, 1))
+    first_september = pytz.timezone("Asia/Yekaterinburg").localize(datetime(current_date.year, 9, 1))
 
     if current_date < first_september:  # Если следующий учебный год не наступил, то считаем что сейчас предыдущий год.
         first_september = datetime(current_date.year - 1, 9, 1)
@@ -21,7 +21,7 @@ def get_first_day_of_first_september_week(current_date: datetime):
 
 def get_date_from_schedule(first_day_of_first_september_week: datetime, week: int, day_of_week: str, time: str):
     index_day_of_week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"].index(day_of_week)
-    hours, minutes = map(int, time.split(':'))
+    hours, minutes = map(int, time.split(":"))
 
     return first_day_of_first_september_week + timedelta(weeks=week - 1, days=index_day_of_week, hours=hours, minutes=minutes)
 
@@ -40,31 +40,43 @@ def get_lesson_num(lesson_start_time: str):
         "22:55": "10"
     }
 
+    lesson_num = lesson_times.get(lesson_start_time)
+    if lesson_num is None:
+        print(f"Ошибка — Время '{lesson_num}' не найдено в словаре")
+
     return lesson_times.get(lesson_start_time)
 
 
 def get_event_color(lesson_type: str):
     event_colors = {
-        'Лекция': '3',
-        'Практика (семинар)': '6',
-        'Лабораторная работа': '9',
-        'Физвоспитание': '2',
-        'Военная подготовка': '10',
-        'Лекция + практика': '4',
-        'Консультация': '7',
-        'Экзамен': '11',
-        'Консультация экзамена': '11',
-        'Ликвидация задолженостей': '11',
-        'Зачёт с оценкой': '11',
-        'Зачёт': '11',
-        'Защита (Курсовой/РГР/Лабораторной)': '11',
-        'Лекция + практика + лабораторная работа': '1',
-        'Мероприятие': '5',
-        'Кураторский час': '5',
-        'Прочее': '8',
+        "Лекция": "3",
+        "Практика (семинар)": "6",
+        "Лабораторная работа": "9",
+        "Физвоспитание": "2",
+        "Военная подготовка": "10",
+        "Лекция + практика": "4",
+        "Консультация": "7",
+        "Экзамен": "11",
+        "Консультация экзамена": "11",
+        "Ликвидация задолженостей": "11",
+        "Зачёт с оценкой": "11",
+        "Зачёт": "11",
+        "Защита (Курсовой/РГР/Лабораторной)": "11",
+        "Лекция + практика + лабораторная работа": "1",
+        "Мероприятие": "5",
+        "Кураторский час": "5",
+        "Прочее": "8",
     }
 
+    event_color = event_colors.get(lesson_type)
+    if event_color is None:
+        print(f"Ошибка — Тип предмета '{lesson_type}' не найден в словаре")
+
     return event_colors.get(lesson_type)
+
+
+def format_event_as_string(event: Event):
+    return f"{datetime.strftime(event.start, '%d.%m %H:%M')}, {event.summary}, {event.location}, {event.description}"
 
 
 # Main Code
@@ -84,22 +96,22 @@ def get_schedule_events(schedule_semester_id: int, schedule_type: int, student_g
 
     response = requests.get("https://isu.ugatu.su/api/new_schedule_api/", params)
     soup = BeautifulSoup(response.text, "html.parser")
-    lesson_rows = soup.find('tbody').findAll('tr')
+    lesson_rows = soup.find("tbody").findAll("tr")
 
     schedule_events = {}
 
-    current_date = datetime.now(pytz.timezone('Asia/Yekaterinburg'))
+    current_date = datetime.now(pytz.timezone("Asia/Yekaterinburg"))
     first_day_of_first_september_week = get_first_day_of_first_september_week(current_date)
 
     day_of_week = ""
     first_lesson = set()
     for lesson_row in lesson_rows:
-        lesson_columns = lesson_row.findAll('td')
+        lesson_columns = lesson_row.findAll("td")
 
-        if 'dayheader' in lesson_row['class']:
+        if "dayheader" in lesson_row["class"]:
             day_of_week = lesson_columns[0].text
 
-        if 'noinfo' in lesson_row['class']:
+        if "noinfo" in lesson_row["class"]:
             continue
 
         lesson_start_time, lesson_end_time = lesson_columns[1].text.split("-")
@@ -119,9 +131,9 @@ def get_schedule_events(schedule_semester_id: int, schedule_type: int, student_g
             lesson_start_date = get_date_from_schedule(first_day_of_first_september_week, int(week), day_of_week, lesson_start_time)
 
             schedule_events[lesson_start_date] = Event(
-                    f'{get_lesson_num(lesson_start_time)}. {lesson_name} — {lesson_type}',
-                    description=("Преподаватель" if schedule_type == 1 else "Группа") + f': {lesson_teacher_or_student_group}\n' +
-                                (f'Комментарий: {lesson_comment}\n' if lesson_comment != '' else ''),
+                    f"{get_lesson_num(lesson_start_time)}. {lesson_name} — {lesson_type}",
+                    description=("Преподаватель" if schedule_type == 1 else "Группа") + f": {lesson_teacher_or_student_group}" +
+                                (f"\nКомментарий: {lesson_comment}" if lesson_comment != "" else ""),
                     minutes_before_popup_reminder=(minutes_before_popup_reminder if week + day_of_week in first_lesson else minutes_before_popup_reminder_first_lesson),
                     color_id=get_event_color(lesson_type),
                     location=lesson_classroom,
@@ -135,12 +147,12 @@ def get_schedule_events(schedule_semester_id: int, schedule_type: int, student_g
     return schedule_events
 
 
-if __name__ == "__main__":
+def main():
     config = configparser.ConfigParser()
     config.read("settings.ini")
 
     gc = GoogleCalendar(config["Settings"]["default_calendar"], credentials_path="client_secret.json")
-    gc_events = list(gc.get_events(timezone='Asia/Yekaterinburg'))
+    gc_events = list(gc.get_events(timezone="Asia/Yekaterinburg"))
 
     schedule_events = get_schedule_events(
         int(config["Settings"]["schedule_semester_id"]),
@@ -153,7 +165,7 @@ if __name__ == "__main__":
     for gc_event in gc_events:
         if gc_event.start not in schedule_events:  # Если в календаре есть начало занятия, а в расписании нет, то считаем что пара удалена.
             gc.delete_event(gc_event, send_updates="all")
-            print("Удалён —", gc_event)
+            print(f"Занятие удаленно — {format_event_as_string(gc_event)}")
             continue
 
         schedule_event = schedule_events[gc_event.start]
@@ -161,13 +173,17 @@ if __name__ == "__main__":
         is_event_updated = (gc_event.summary != schedule_event.summary or gc_event.description != schedule_event.description or gc_event.location != schedule_event.location or
                             gc_event.reminders != schedule_event.reminders)
 
-        if is_event_updated:  # Если есть различия в расписании и календаре, обновляем событие.
+        if is_event_updated:  # Если есть различия у занятия в расписании и календаре, обновляем событие.
             schedule_event.event_id = gc_event.event_id
             gc.update_event(schedule_event, send_updates="all")
-            print("Обновлён —", gc_event, schedule_event)
+            print(f"Занятие обновлено — {format_event_as_string(gc_event)} -> {format_event_as_string(schedule_event)}")
 
-        schedule_events.pop(gc_event.start)  # Обязательно удаляем из списка расписания занятия, которые уже были в календаре.
+        schedule_events.pop(gc_event.start)  # Удаляем из списка расписания занятия, которые уже были в календаре.
 
     for schedule_event in schedule_events.values():
         gc.add_event(schedule_event, send_updates="all")
-        print("Добавлен —", schedule_event)
+        print(f"Занятие добавлено — {format_event_as_string(schedule_event)}")
+
+
+if __name__ == "__main__":
+    main()
