2fc800f6b22b6dd96103bf1d870dd801bea7ad1b
GuFFy12 <jokers_hello_0d@icloud.com>
2023-10-31 17:08:58 +0500

    Add run scheduler; Fix error for adding a single event when two events occur at the same time on the schedule.

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+import configparser
+import time
+import traceback
+from datetime import datetime, timedelta
 from typing import Optional
 
-import requests
-import configparser
 import pytz
-from datetime import datetime, timedelta
+import requests
 from bs4 import BeautifulSoup
 from gcsa.event import Event
 from gcsa.google_calendar import GoogleCalendar
 
 
 # Utilities
+
+def every(delay, task, args):
+    next_time = time.time() + delay
+    while True:
+        time.sleep(max(0, next_time - time.time()))
+        try:
+            task(*args)
+        except Exception:
+            traceback.print_exc()
+
+        next_time += (time.time() - next_time) // delay * delay + delay
+
 
 def get_first_day_of_first_september_week(current_date: datetime):
     first_september = pytz.timezone("Asia/Yekaterinburg").localize(datetime(current_date.year, 9, 1))
@@ -22,9 +36,9 @@ def get_first_day_of_first_september_week(current_date: datetime):
     return first_september - timedelta(days=day_of_week)
 
 
-def get_date_from_schedule(first_day_of_first_september_week: datetime, week: int, day_of_week: str, time: str):
+def get_date_from_schedule(first_day_of_first_september_week: datetime, week: int, day_of_week: str, lesson_time: str):
     index_day_of_week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"].index(day_of_week)
-    hours, minutes = map(int, time.split(":"))
+    hours, minutes = map(int, lesson_time.split(":"))
 
     return first_day_of_first_september_week + timedelta(weeks=week - 1, days=index_day_of_week, hours=hours, minutes=minutes)
 
@@ -62,7 +76,11 @@ def get_event_color(lesson_type: str):
 
 
 def format_event_as_string(event: Event):
-    return f"{datetime.strftime(event.start, '%d.%m %H:%M')}, {event.summary}, {event.location}, {event.description}"
+    return f"{datetime.strftime(event.start, '%d.%m.%Y %H:%M')}, {event.summary}, {event.location}, {event.description}"
+
+
+def get_event_hash(event: Event):
+    return str(hash((event.summary, event.description, event.location, datetime.strftime(event.start, '%d.%m.%Y %H:%M'))))
 
 
 # Main Code
@@ -90,7 +108,7 @@ def get_schedule_events(schedule_semester_id: int, schedule_type: int, student_g
     first_day_of_first_september_week = get_first_day_of_first_september_week(current_date)
 
     day_of_week = ""
-    first_lesson = set()
+    day_first_lesson_number = {}
     for lesson_row in lesson_rows:
         lesson_columns = lesson_row.findAll("td")
 
@@ -109,18 +127,24 @@ def get_schedule_events(schedule_semester_id: int, schedule_type: int, student_g
         lesson_comment = lesson_columns[7].text
 
         for week in lesson_weeks:
+            lesson_day_hash = str(hash((week, day_of_week)))
+            lesson_number = get_lesson_num(lesson_start_time)
+
             lesson_end_date = get_date_from_schedule(first_day_of_first_september_week, int(week), day_of_week, lesson_end_time)
             if current_date > lesson_end_date:  # Не добавляем уже прошедшие занятия.
-                first_lesson.add(week + day_of_week)
+                if lesson_day_hash not in day_first_lesson_number:
+                    day_first_lesson_number[lesson_day_hash] = lesson_number
                 continue
 
             lesson_start_date = get_date_from_schedule(first_day_of_first_september_week, int(week), day_of_week, lesson_start_time)
 
-            schedule_events[lesson_start_date] = Event(
-                f"{get_lesson_num(lesson_start_time)}. {lesson_name} — {lesson_type}",
+            schedule_event = Event(
+                f"{lesson_number}. {lesson_name} — {lesson_type}",
                 description=("Преподаватель" if schedule_type == 1 else "Группа") + f": {lesson_teacher_or_student_group}" +
                             (f"\nКомментарий: {lesson_comment}" if lesson_comment != "" else ""),
-                minutes_before_popup_reminder=(minutes_before_popup_reminder if week + day_of_week in first_lesson else minutes_before_popup_reminder_first_lesson),
+                minutes_before_popup_reminder=(minutes_before_popup_reminder_first_lesson
+                                               if (lesson_day_hash not in day_first_lesson_number or day_first_lesson_number[lesson_day_hash] == lesson_number)
+                                               else minutes_before_popup_reminder),
                 color_id=get_event_color(lesson_type),
                 location=lesson_classroom,
                 timezone="Asia/Yekaterinburg",
@@ -128,16 +152,15 @@ def get_schedule_events(schedule_semester_id: int, schedule_type: int, student_g
                 end=lesson_end_date
             )
 
-            first_lesson.add(week + day_of_week)
+            schedule_events[get_event_hash(schedule_event)] = schedule_event
+
+            if lesson_day_hash not in day_first_lesson_number:
+                day_first_lesson_number[lesson_day_hash] = lesson_number
 
     return schedule_events
 
 
-def main():
-    config = configparser.ConfigParser()
-    config.read("settings.ini")
-
-    gc = GoogleCalendar(config["Settings"]["default_calendar"], credentials_path="client_secret.json")
+def sync_calendar_with_schedule(config: configparser.ConfigParser, gc: GoogleCalendar):
     gc_events = list(gc.get_events(timezone="Asia/Yekaterinburg"))
 
     schedule_events = get_schedule_events(
@@ -150,12 +173,12 @@ def main():
     )
 
     for gc_event in gc_events:
-        if gc_event.start not in schedule_events:  # Если в календаре есть начало занятия, а в расписании нет, то считаем что пара удалена.
+        if get_event_hash(gc_event) not in schedule_events:  # Если в календаре хеш события, а в расписании нет, то считаем что пара удалена.
             gc.delete_event(gc_event, send_updates="all")
             print(f"Занятие удаленно — {format_event_as_string(gc_event)}")
             continue
 
-        schedule_event = schedule_events[gc_event.start]
+        schedule_event = schedule_events[get_event_hash(gc_event)]
 
         is_event_updated = (gc_event.summary != schedule_event.summary or gc_event.description != schedule_event.description or gc_event.location != schedule_event.location or
                             gc_event.reminders != schedule_event.reminders)
@@ -163,13 +186,27 @@ def main():
         if is_event_updated:  # Если есть различия у занятия в расписании и календаре, обновляем событие.
             schedule_event.event_id = gc_event.event_id
             gc.update_event(schedule_event, send_updates="all")
-            print(f"Занятие обновлено — {format_event_as_string(gc_event)} -> {format_event_as_string(schedule_event)}")
+            print(f"Занятие обновлено — {format_event_as_string(schedule_event)}")
 
-        schedule_events.pop(gc_event.start)  # Удаляем из списка расписания занятия, которые уже были в календаре.
+        schedule_events.pop(get_event_hash(gc_event))  # Удаляем из списка расписания занятия, которые уже были в календаре.
 
     for schedule_event in schedule_events.values():
         gc.add_event(schedule_event, send_updates="all")
         print(f"Занятие добавлено — {format_event_as_string(schedule_event)}")
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read("settings.ini")
+
+    gc = GoogleCalendar(config["Settings"]["default_calendar"], credentials_path="client_secret.json")
+
+    sync_calendar_with_schedule(config, gc)
+
+    if config["Settings"]["task_scheduler_delay"] == "0":
+        return
+
+    every(int(config["Settings"]["task_scheduler_delay"]), sync_calendar_with_schedule, (config, gc))
 
 
 if __name__ == "__main__":
